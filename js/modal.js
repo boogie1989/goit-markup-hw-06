ec8b72a8e0f56e957041387be3f5ad3e16e585be
boogie1989 <sergeybugai@gmail.com>
2024-03-24 14:52:53 +0200

    review fixes

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,4 @@
-(() => {
-	const toggle = () => document.querySelector("[data-modal]").classList.toggle("is-hidden");
-	for (const el of [document.querySelector("[data-modal-open]"), document.querySelector("[data-modal-close]")]) {
-		el.addEventListener("click", toggle);
-	}
-
-	document.querySelector(".backdrop").addEventListener("click", (event) => {
-		if (event.target != document.querySelector(".modal")) {
-			toggle();
-		}
-	});
-})();
+const toggle = () => document.querySelector("[data-modal]").classList.toggle("is-hidden");
+for (const atr of ["[data-modal-open]", "[data-modal-close]"]) {
+	document.querySelector(atr).addEventListener("click", toggle);
+}
